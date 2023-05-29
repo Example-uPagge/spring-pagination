b62eda5612cd8a491afc4fb45468b070d0d4791a
Struchkov Mark <mark@struchkov.dev>
2023-05-29 19:42:29 +0300

    Переход на SpringBoot 3 и добавление примера со Slice

--- src/main/java/dev/struchkov/example/PostController.java
+++ src/main/java/dev/struchkov/example/PostController.java
@@ -1,15 +1,15 @@
 package dev.struchkov.example;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("api/post")
@@ -24,6 +24,14 @@ public class PostController {
             @RequestParam(value = "limit", defaultValue = "20") Integer limit
     ) {
         return repository.findAll(PageRequest.of(offset, limit));
+    }
+
+    @GetMapping("exampleSlice")
+    public Slice<Post> getAllSlice(
+            @RequestParam(value = "offset", defaultValue = "0") @Min(0) Integer offset,
+            @RequestParam(value = "limit", defaultValue = "20") Integer limit
+    ) {
+        return repository.findAllSlice(PageRequest.of(offset, limit));
     }
 
     @GetMapping("exampleSort")
--- src/main/java/dev/struchkov/example/PostRepository.java
+++ src/main/java/dev/struchkov/example/PostRepository.java
@@ -2,6 +2,7 @@ package dev.struchkov.example;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    @Query("SELECT p FROM Post p")
+    Slice<Post> findAllSlice(Pageable pageable);
 
     Page<Post> findAllByTitleLikeIgnoreCase(String titleLike, Pageable pageable);
 
